d83583b3ce5f180dfd80f5c5dc520e101ff8a26e
m.dorokhov <m.dorokhov@bftcom.com>
2025-03-26 10:48:49 +0300

    lesson5

---
+++ src/main/java/lesson5/Test.java
@@ -0,0 +1,22 @@
+package lesson5;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите сумму вклада: ");
+        float depositAmount = scanner.nextFloat();
+
+        System.out.print("Введите срок вклада в месяцах: ");
+        int months = scanner.nextInt();
+
+        Bank bank = new Bank(depositAmount, months);
+        float finalAmount = bank.getFinalAmountByFor();
+        float finalAmount2 = bank.getFinalAmountByWhile();
+
+        System.out.printf("После %d месяцев сумма вклада составит %f%n", months, finalAmount);
+        System.out.printf("После %d месяцев сумма вклада составит %f%n", months, finalAmount2);
+    }
+}
